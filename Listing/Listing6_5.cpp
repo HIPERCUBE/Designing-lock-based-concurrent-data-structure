93c927fb3c613b5f44b5b120c57b1b23af5c4feb
Joowon Ryoo <joowonryoo@gmail.com>
2016-01-14 01:22:22 +0900

    Update Listing6_5
    Add Listing6_5 Source

--- Listing/Listing6_5.cpp
+++ Listing/Listing6_5.cpp
@@ -8,3 +8,45 @@
 // 그래서 head->next와 tail->next사이에 경쟁이 없게된다.
 // 단점은 더미 노드들을 위해 포인터로 데이터를 저장하는 추가의 인다이렉션이 들어간다는 것이다.
 // 아래에서 어떻게 구현하는제 보게될것이다.
+
+
+#include <memory>
+
+template<typename T>
+class queue {
+private:
+    struct node {
+/* 1 */ std::shared_ptr<T> data;
+        std::unique_ptr<node> next;
+    };
+
+    std::unique_ptr<node> head;
+    node* tail;
+
+public:
+    /* 2 */
+    queue() : head(new node), tail(head.get()) { }
+
+    queue(const queue& other) = delete;
+
+    queue& operator=(const queue& other) = delete;
+
+    std::shared_ptr<T> try_pop() {
+/* 3 */ if (head.get() == tail) {
+            return std::shared_ptr<T>();
+        }
+/* 4 */ std::shared_ptr<T> const res(head->data);
+        std::unique_ptr<node> old_head = std::move(head);
+/* 5 */ head = std::move(old_head->next);
+/* 6 */ return res;
+    }
+
+    void push(T new_value) {
+/* 7 */ std::shared_ptr<T> new_data(std::make_shared<T>(std::move(new_value)));
+/* 8 */ std::unique_ptr<node> p(new node);
+/* 9 */ tail->data = new_data;
+        node* const new_tail = p.get();
+        tail->next = std::move(p);
+        tail = new_tail;
+    }
+};
